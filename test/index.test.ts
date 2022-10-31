d92ff2f82ec974d1d3ee1ebc5d07e83ebd2ec28f
KaKa <kaka@kakawebsitedemo.com>
2022-10-31 15:59:23 +0800

    test: update test case

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,31 @@
+import Fastify from 'fastify'
 import t from 'tap'
-import * as lib from '../lib'
+import plugin from '../lib'
 
-t.test('import', function (t) {
+t.test('invalid api key - no option', function (t) {
   t.plan(1)
-  t.same(typeof lib, 'object')
+  const fastify = Fastify()
+  t.rejects(async () => {
+    await fastify.register(plugin)
+  })
 })
+
+const invalid = [
+  0,
+  true,
+  undefined,
+  null,
+  {},
+  []
+]
+
+for (const apiKey of invalid) {
+  t.test(`invalid api key - ${typeof apiKey}`, function (t) {
+    t.plan(1)
+    const fastify = Fastify()
+    t.rejects(async () => {
+    // @ts-expect-error
+      await fastify.register(plugin, { apiKey })
+    })
+  })
+}
